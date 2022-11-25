ce1e2941a7e3335d9940ffa01f1dd95fe7baf347
Jeremia <Jeremia.faust@snhu.edu>
2022-11-25 11:54:19 -0500

    wrong folder

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs =require('hbs');
-require('./app_api/database/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -14,7 +13,6 @@ var roomsRouter = require('./app_server/routes/rooms');
 var mealsRouter = require('./app_server/routes/meals');
 var aboutRouter = require('./app_server/routes/about');
 var contactRouter = require('./app_server/routes/contact');
-var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -39,7 +37,6 @@ app.use('/rooms', roomsRouter);
 app.use('/meals', mealsRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
-app.use('/api',apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
